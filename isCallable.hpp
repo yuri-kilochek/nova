5ecdbbf68c38db2eef96ac634a3b43f8ac0f3bd4
yuri_kilochek <yuri.kilochek@gmail.com>
2013-12-22 16:58:56 +0400

    Added isCallable.

---
+++ isCallable.hpp
@@ -0,0 +1,34 @@
+#ifndef NOVA_INCLUDE_GUARD_IS_CALLABLE_HPP
+#define NOVA_INCLUDE_GUARD_IS_CALLABLE_HPP
+
+#include "mock.hpp"
+#include "Bool.hpp"
+#include "isSameAs.hpp"
+
+namespace nova {
+    namespace internals {
+        template <typename Type, typename... Args>
+        class IsCallable {
+            private:
+                template <typename OtherType>
+                struct Holder {};
+
+                struct Yes {};
+                struct No {};
+
+                template <typename OtherType>
+                static auto test(Holder<OtherType>) -> decltype(mock<OtherType>()(mock<Args>()...), Yes());
+                static auto test(...) -> No;
+
+            public:
+                static constexpr Bool value = isSameAs<decltype(test(Holder<Type>())), Yes>();
+        };
+    }
+
+    template <typename Type, typename... Args>
+    inline constexpr Bool isCallable() {
+        return internals::IsCallable<Type, Args...>::value;
+    }
+}
+
+#endif
