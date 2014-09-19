a300cc022ea8f2750d1c6c2f45dddc956178deb0
yuri_kilochek <yuri.kilochek@gmail.com>
2014-09-20 03:26:48 +0400

    Made min/max inline and constexpr.

--- min-max.hpp
+++ min-max.hpp
@@ -5,32 +5,32 @@
 
 namespace nova {
     template <typename Value>
-    Value min(Value&& value) {
+    inline constexpr Value min(Value&& value) {
         return forward<Value>(value);
     }
 
     template <typename First, typename Second>
-    auto min(First&& first, Second&& second) {
+    inline constexpr auto min(First&& first, Second&& second) {
         return (second < first) ? forward<Second>(second) : forward<First>(first);
     }
 
     template <typename First, typename... Rest>
-    auto min(First&& first, Rest&&... rest) {
+    inline constexpr auto min(First&& first, Rest&&... rest) {
         return min(forward<First>(first), min(forward<Rest>(rest)...));
     }
 
     template <typename Value>
-    Value max(Value&& value) {
+    inline constexpr Value max(Value&& value) {
         return forward<Value>(value);
     }
 
     template <typename First, typename Second>
-    auto max(First&& first, Second&& second) {
+    inline constexpr auto max(First&& first, Second&& second) {
         return (first < second) ? forward<Second>(second) : forward<First>(first);
     }
 
     template <typename First, typename... Rest>
-    auto max(First&& first, Rest&&... rest) {
+    inline constexpr auto max(First&& first, Rest&&... rest) {
         return max(forward<First>(first), max(forward<Rest>(rest)...));
     }
 }
