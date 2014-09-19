812abecdf525e022ad791239f2e37638e2418d45
yuri_kilochek <yuri.kilochek@gmail.com>
2014-09-19 13:22:27 +0400

    Decided bytes don't need literals after all.

--- Byte.hpp
+++ Byte.hpp
@@ -5,14 +5,6 @@
 
 namespace nova {
     using Byte = UInt8;
-
-    inline constexpr Byte operator "" _b(unsigned long long value) {
-        return static_cast<Byte>(value);
-    }
-
-    constexpr Byte min_b = min_ui8;
-
-    constexpr Byte max_b = max_ui8;
 }
 
 #endif
--- SByte.hpp
+++ SByte.hpp
@@ -5,14 +5,6 @@
 
 namespace nova {
     using SByte = Int8;
-
-    inline constexpr SByte operator "" _sb(unsigned long long value) {
-        return static_cast<SByte>(value);
-    }
-
-    constexpr SByte min_sb = min_i8;
-
-    constexpr SByte max_sb = max_i8;
 }
 
 #endif
