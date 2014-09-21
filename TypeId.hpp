f3b5e9c757a7a92b2ab76a2325ef108ad46658ce
yuri_kilochek <yuri.kilochek@gmail.com>
2014-09-21 19:27:00 +0400

    TypeId's are now obtained via functions, not macro.

--- TypeId.hpp
+++ TypeId.hpp
@@ -4,21 +4,24 @@
 #include "API.hpp"
 #include "Bool.hpp"
 #include "EnableIf.hpp"
-#include "isSame.hpp"
+#include "areSame.hpp"
 #include "UInt.hpp"
 
 #include <typeinfo>
 
 namespace nova {
-    class TypeId;
-
-    namespace internals {
-        constexpr TypeId typeId(::std::type_info const& typeInfo);
-    }
-
     class NOVA_API TypeId final {
-        friend constexpr TypeId internals::typeId(::std::type_info const& typeInfo);
         public:
+            template <typename Type>
+            friend TypeId typeId() {
+                return {typeid(Type)};
+            };
+
+            template <typename Type>
+            friend TypeId typeId(Type const& object) {
+                return {typeid(object)};
+            };
+
             friend Bool operator==(TypeId const& a, TypeId const& b) {
                 return *a.typeInfoPtr == *b.typeInfoPtr;
             }
@@ -43,34 +46,18 @@ namespace nova {
                 return !(a < b);
             }
 
-            template <typename MaybeTypeId, EnableIf<isSame<MaybeTypeId, TypeId>()>...>
+            template <typename MaybeTypeId, EnableIf<areSame<MaybeTypeId, TypeId>()>...>
             friend UInt hash(MaybeTypeId const& typeId) {
-                return typeId.typeInfoPtr->hash_code();
+                return (*typeId.typeInfoPtr).hash_code();
             }
 
         private:
             ::std::type_info const* typeInfoPtr;
 
-            constexpr TypeId(::std::type_info const& typeInfo)
+            TypeId(::std::type_info const& typeInfo)
                 : typeInfoPtr(&typeInfo)
             {}
     };
-
-    namespace internals {
-        inline constexpr TypeId typeId(::std::type_info const& typeInfo) {
-            return {typeInfo};
-        }
-    }
 }
-
-#define NOVA_TYPE_ID(...) (                                        \
-    [&]{                                                           \
-        try {                                                      \
-            return ::nova::internals::typeId(typeid(__VA_ARGS__)); \
-        } catch (::std::bad_typeid&) {                             \
-            /* TODO: panic here */                                 \
-        }                                                          \
-    }()                                                            \
-)
 
 #endif
