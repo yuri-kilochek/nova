8511efe7ce1cca45d07f6a96ce2736311b362cda
yuri_kilochek <yuri.kilochek@gmail.com>
2014-09-22 03:57:42 +0400

    Made storageOf able to deal with null pointers.

--- storageOf.hpp
+++ storageOf.hpp
@@ -3,20 +3,17 @@
 
 #include "EnableIf.hpp"
 #include "isPolymorphic.hpp"
-#include "ASSERT.hpp"
 #include "CopyCV.hpp"
 #include "Void.hpp"
 
 namespace nova {
     template <typename Type, EnableIf<isPolymorphic<Type>()>...>
     inline auto storageOf(Type* object) {
-        NOVA_ASSERT(object);
         return dynamic_cast<CopyCV<Void, Type>*>(object);
     }
 
     template <typename Type, EnableIf<!isPolymorphic<Type>()>...>
     inline auto storageOf(Type* object) {
-        NOVA_ASSERT(object);
         return static_cast<CopyCV<Void, Type>*>(object);
     }
 }
