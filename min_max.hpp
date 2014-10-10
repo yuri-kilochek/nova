8174db0a63ae2cb4bf75aaf69647cad178e36763
yuri_kilochek <yuri.kilochek@gmail.com>
2014-10-10 12:52:49 +0400

    Made min/max more error friendly.

--- min_max.hpp
+++ min_max.hpp
@@ -10,12 +10,16 @@ namespace nova {
     }
 
     template <typename First, typename Second>
-    inline constexpr auto min(First&& first, Second&& second) {
+    inline constexpr auto min(First&& first, Second&& second)
+        -> decltype((second < first) ? forward<Second>(second) : forward<First>(first))
+    {
         return (second < first) ? forward<Second>(second) : forward<First>(first);
     }
 
     template <typename First, typename... Rest>
-    inline constexpr auto min(First&& first, Rest&&... rest) {
+    inline constexpr auto min(First&& first, Rest&&... rest)
+        -> decltype(min(forward<First>(first), min(forward<Rest>(rest)...)))
+    {
         return min(forward<First>(first), min(forward<Rest>(rest)...));
     }
 
@@ -25,12 +29,16 @@ namespace nova {
     }
 
     template <typename First, typename Second>
-    inline constexpr auto max(First&& first, Second&& second) {
+    inline constexpr auto max(First&& first, Second&& second)
+        -> decltype((first < second) ? forward<Second>(second) : forward<First>(first))
+    {
         return (first < second) ? forward<Second>(second) : forward<First>(first);
     }
 
     template <typename First, typename... Rest>
-    inline constexpr auto max(First&& first, Rest&&... rest) {
+    inline constexpr auto max(First&& first, Rest&&... rest)
+        -> decltype(max(forward<First>(first), max(forward<Rest>(rest)...)))
+    {
         return max(forward<First>(first), max(forward<Rest>(rest)...));
     }
 }
