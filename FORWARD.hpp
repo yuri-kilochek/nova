7791c8c0e5fa519a84aaee2a08a10cf78e037560
yuri_kilochek <yuri.kilochek@gmail.com>
2015-01-18 18:31:38 +0300

    forward is now NOVA_FORWARD, removing redundancy.

--- FORWARD.hpp
+++ FORWARD.hpp
@@ -1,5 +1,5 @@
-#ifndef NOVA_HEADER_forward
-#define NOVA_HEADER_forward
+#ifndef NOVA_HEADER_FORWARD
+#define NOVA_HEADER_FORWARD
 
 #include "StripRef.hpp"
 #include "EnableIf.hpp"
@@ -7,14 +7,17 @@
 
 namespace nova {
     template <typename Arg>
-    inline constexpr Arg&& forward(StripRef<Arg>& arg) {
+    inline constexpr Arg&& _internal_FORWARD(StripRef<Arg>& arg) {
         return static_cast<Arg&&>(arg);
     }
 
     template <typename Arg, EnableIf<!isLValueRef<Arg>()>...>
-    inline constexpr Arg&& forward(StripRef<Arg>&& arg) {
+    inline constexpr Arg&& _internal_FORWARD(StripRef<Arg>&& arg) {
         return static_cast<Arg&&>(arg);
     }
 }
+
+#define NOVA_FORWARD(arg) \
+    ::nova::_internal_FORWARD<decltype(arg)>(arg)
 
 #endif
