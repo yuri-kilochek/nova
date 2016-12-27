f3675ed9036ebe7e3730f097c68012dc80aabbb5
Yuri Kilochek <yuri.kilochek@gmail.com>
2016-12-27 23:43:13 +0300

    Typo fix.

--- Float.hpp
+++ Float.hpp
@@ -8,7 +8,7 @@ namespace nova {
     using Float = decltype(0.0);
 
     static_assert(::std::numeric_limits<float>::is_iec559, "float does not conform to IEC 60559.");
-    static_assert(sizeof(float) * CHAR_BIT == 32, "float is not IEC 60559 binary64.");
+    static_assert(sizeof(float) * CHAR_BIT == 32, "float is not IEC 60559 binary32.");
     using Float32 = float;
 
     static_assert(::std::numeric_limits<double>::is_iec559, "double does not conform to IEC 60559.");
